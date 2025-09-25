b3ce9703767460d7f86ac3a9d2d5541ad420b6df
janussyrak-specialisterne <janus.hansen@specialisterne.com>
2025-09-25 10:50:52 +0200

    Add comments

--- delopgave3.py
+++ delopgave3.py
@@ -6,20 +6,24 @@ def read_and_write(source_file: str, destination_file: str) -> bool:
     src = Path(source_file)
     dst = Path(destination_file)
 
+    # Verify existence of source file
     if not src.exists():
         print("The source file does not exist. Please ensure that the file name is spelt correctly.")
         return False
 
+    # Verify extension compatibility
     if src.suffix != dst.suffix:
         print("The file extensions are not compatible. Please check for potential misspellings.")
         return False
 
+    # Read with try-catch
     try:
         df = pd.read_csv(source_file)
     except PermissionError:
         print("Permission error: cannot read the source file. Check file permissions.")
         return False
 
+    # Ensure destination file is not overwritten
     if dst.exists():
         print("The destination file already exists.")
     df.to_csv(destination_file)
